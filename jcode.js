437737c56860dc0c3ab499ce6ac02eba40fc80fb
Toby Hughes <tobywhughes@gmail.com>
2016-11-23 16:48:26 -0800

    Made it more AESTHETIC

--- jcode.js
+++ jcode.js
@@ -1,10 +1,10 @@
 var rotation = function (){
   $("#vapor-dude").rotate({
-    angle:0,
+    angle:-1,
     animateTo:360,
     callback: rotation,
     easing: function (x,t,b,c,d){  
-      return c*(t/d)+b;
+      return c*(t/(d))+ b;
     }
   });
 }
@@ -12,6 +12,7 @@ var rotation = function (){
 $(function() {
     $( "#panel" ).draggable();
     $( "#panel2" ).draggable();
+    $( "#panel3" ).draggable();
     $( "#vapor-dude" ).draggable();
     $( "#vwave-video" ).draggable();
 
